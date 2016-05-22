d0ebce9525e03a01804fa60cf5682749ec495029
Alexander Melnikov <inzheneher@ya.ru>
2016-05-22 17:39:27 +0600

    2016-05-22

---
+++ com.melnikov.composite.plugin/src/com/melnikov/composite/plugin/ILaunchConfigurationTreeNode.java
@@ -0,0 +1,18 @@
+package com.melnikov.composite.plugin;
+
+import org.eclipse.swt.graphics.Image;
+
+interface ILaunchConfigurationTreeNode {
+	ILaunchConfigurationTreeNode[] getChildren();
+	ILaunchConfigurationTreeNode getParent();
+	boolean hasChildren();
+	boolean hasSelectedChildren();	
+
+	Image getImage();
+	String getText();
+
+	boolean isChecked();
+	boolean isGrayed();
+
+	void setCheckState(boolean state);
+}
